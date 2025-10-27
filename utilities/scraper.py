fccf7ecb530955fdc73ec24c3c5366ca57c2e9b4
Stephen Tanksley <stephen.tanksley@gmail.com>
2025-10-27 10:36:45 -0500

    Fix: Changed scraper model to only focus on URLs instead of headlines, pull keywords directly from URL.

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -15,14 +15,14 @@ MAX_CONCURRENT_PAGES = 5
     This module scrapes from news organizations to extract raw data from them - headlines and URLs
 """
 
-async def fetch_html(context: BrowserContext, url: str, sem: Semaphore) -> list[dict[str, str]]:
+async def fetch_html(context: BrowserContext, url: str, sem: Semaphore) -> dict[str, list[str]]:
     """Fetch all links + headline text from a single page."""
     async with sem:  # limit concurrency to only as many sources as we define in MAX_CONCURRENT_PAGES
         start_time = perf_counter()
         print(f"{datetime.now()} || [{start_time:.3f}] starting {url}")
 
         links = []
-
+        seen = set()
         page = await context.new_page()
         try:
             await page.goto(url, wait_until="domcontentloaded")
@@ -39,6 +39,10 @@ async def fetch_html(context: BrowserContext, url: str, sem: Semaphore) -> list[
                 # Normalize relative URLs
                 href = urljoin(url, href)
 
+                # We don't want to add duplicate URLs
+                if href in seen:
+                    continue
+
                 # Filter out non-HTTP links
                 if not href.startswith("http"):
                     continue
@@ -47,10 +51,28 @@ async def fetch_html(context: BrowserContext, url: str, sem: Semaphore) -> list[
                 if not text:
                     continue
 
+                if len(text.split(" ")) < 8:
+                    continue
+
+                # Filtering conditions - If the URL ends with a trailing slash, get rid of the slash
+                if href[-1] == '/':
+                    href = href.rstrip('/')
+
+                # Filtering conditions - If the URL ends with numbers, strip the numbers out
+                if href[-1].isnumeric():
+                    href = href.rsplit('/')[-2]
+
+
+                keywords = href.rsplit("/", maxsplit=1)[-1]
+                keywords = keywords.split('-')
+
+                # Add items - Add the url to seen and then the full object to links
+                seen.add(href)
                 links.append({
                     "url": href,
-                    "headline": text,
-                    "timestamp": str(datetime.now(timezone.utc))
+                    # "headline": text,
+                    "timestamp": str(datetime.now(timezone.utc)),
+                    "keywords": keywords
                 })
 
             end_time = perf_counter()
@@ -61,9 +83,7 @@ async def fetch_html(context: BrowserContext, url: str, sem: Semaphore) -> list[
         finally:
             await page.close()
 
-        return links
-
-
+        return {url: links}
 
 
 async def main(outlet_urls: list[str] = None, results_dict: dict = None) -> None:
@@ -87,13 +107,18 @@ async def main(outlet_urls: list[str] = None, results_dict: dict = None) -> None
         await browser.close()
 
     # Display a summary of results
-    for site, links in zip(urls, all_results):
-        if site not in results_dict:
-            results_dict[site] = links
+    # for site, links in zip(urls, all_results):
+    #     if site not in results_dict:
+    #         results_dict[site] = links
+
+        for item in all_results:
+            for site, links in item.items():
+                if site not in results_dict:
+                    results_dict[site] = links
 
         print(f"{datetime.now()} || \n🔗 {site}: {len(links)} valid links found")
-        for l in links[:5]:  # print a few sample links per site
-            print(f"  - {l['headline'][:60]} → {l['url']}")
+        for link in links:  # print a few sample links per site
+            print(f"  - {link['url']}")
 
     total_time = perf_counter() - start_time
     print(f"{datetime.now()} || \nTotal execution time: {total_time:.2f}s")
@@ -101,7 +126,7 @@ async def main(outlet_urls: list[str] = None, results_dict: dict = None) -> None
 
 if __name__ == "__main__":
     if f"{date.today()}.json" not in os.listdir("../data/"):
-        print(f"{datetime.now()} || No headlines detected for current date, adding now")
+        print(f"{datetime.now()} || No URLs detected for current date, adding now")
         results = {} # This gets passed in to main
         async_run(main(outlet_urls=urls, results_dict=results))
 
@@ -109,3 +134,18 @@ if __name__ == "__main__":
             json.dump(results, file)
     else:
         print(f"{datetime.now()} || Current headlines found, closing")
+
+# URL classifiers - Turns out a lot of the internet is built on WordPress and the keywords are built into the URLs themselves.
+#   I could just scrape URLs and get relevant keywords from that.
+# GPT4 - Does pretty well off of URLs, not quite free. I could use this to get subjects the overall vibe from the things in the URL itself.
+# Figure out a way of approaching subject "stickiness" in news and how it persists over time. Maybe a SCD2 table which tracks top subjects.
+# Take a look at implementing a clustering algorithm which will take (Combination of NLP and graph theory) - connected components in David's book is worth another read.
+# Take a look at GraphRAG and how it relates to my problem space.
+
+# David makes a distinction between short term, long term memory and a "simulation" capability. Simulation in this case being the idea to "imagine"
+# Check out Cogni and knowledge graphs. How can I use a knowledge graph to get accurate information?
+
+# https://github.com/itsgorain/100DaysOfNLP/blob/master/twitter_trump_hostile_clf_interpretable_extreme.ipynb
+# https://github.com/itsgorain/100DaysOfNLP/blob/master/nlp_different_authors.ipynb
+
+# It would be super cool to see
